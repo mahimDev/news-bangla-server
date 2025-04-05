b506d771a95525b31bb2f2a2066ee8d114bbac92
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-04-06 00:49:07 +0600

    added my some aggregate related work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@newsbangla.k7ko6y9.mongodb.net/?retryWrites=true&w=majority&appName=newsBangla`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,73 @@ async function run() {
     const newsCollection = dataBase.collection("news");
     app.get("/", (req, res) => {
       res.send("Welcome to news bangla");
+    });
+    // latest all news get api
+    app.get("/news", async (req, res) => {
+      const news = await newsCollection
+        .aggregate([
+          {
+            $addFields: {
+              createdAt: {
+                $toDate: "$_id",
+              },
+            },
+          },
+          {
+            $sort: {
+              createdAt: -1,
+            },
+          },
+          {
+            $skip: 1,
+          },
+          {
+            $project: {
+              title: 1,
+              content: 1,
+              imageUrl: 1,
+              createdAt: {
+                $dateToString: {
+                  format: "%Y-%m-%d %H:%M",
+                  date: { $toDate: "$_id" },
+                },
+              },
+            },
+          },
+        ])
+        .toArray();
+      res.send(news);
+    });
+    // latest 1 news get api
+    app.get("/latestNews", async (req, res) => {
+      const latestNews = await newsCollection
+        .aggregate([
+          {
+            $sort: { _id: -1 },
+          },
+          {
+            $limit: 1,
+          },
+          {
+            $addFields: {
+              createdAt: {
+                $dateToString: {
+                  format: "%d-%m-%Y %H:%M",
+                  date: { $toDate: "$_id" },
+                },
+              },
+            },
+          },
+        ])
+        .toArray();
+      res.send(latestNews);
+    });
+    // read a singel news
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await newsCollection.findOne(query);
+      res.send(result);
     });
     // news post api
     app.post("/addNews", async (req, res) => {
