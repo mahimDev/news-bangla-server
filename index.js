748550e7014ebc094b36e8720b684c3d2e63ca49
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-05-01 22:35:30 +0600

    added some changes

--- index.js
+++ index.js
@@ -446,9 +446,9 @@ async function run() {
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -457,5 +457,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log("listening on port", port);
+  // console.log("listening on port", port);
 });
